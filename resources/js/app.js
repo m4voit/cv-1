ac2c76b4598b7457abf838cf25c4e4a8df4d0aa5
Maxim Voitenko <132004271+m4voit@users.noreply.github.com>
2024-05-08 11:19:45 +0600

    add: front

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
+// app.js
 import './bootstrap';
+
+import {createApp} from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(ElementPlus)
+app.mount("#app")
